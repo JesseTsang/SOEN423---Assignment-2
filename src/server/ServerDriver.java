cdd3b1d61be5ab3a6f1f3387c6dfe7fde3bd3af6
Jesse Tsang <Jessetsang@outlook.com>
2017-11-02 15:51:01 -0400

    ServerDriver done.

--- src/server/ServerDriver.java
+++ src/server/ServerDriver.java
@@ -1,38 +1,91 @@
 package server;
 
+import java.util.HashMap;
+import java.util.Properties;
+
+import org.omg.CORBA.ORB;
+import org.omg.CosNaming.NameComponent;
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
+import org.omg.PortableServer.POA;
+import org.omg.PortableServer.POAHelper;
+
+import common.BankServerInterface;
+import common.BankServerInterfaceHelper;
 import domain.BranchID;
 import domain.EditRecordFields;
 
 public class ServerDriver 
 {
 	public static void main(String[] args) 
-	{		
+	{	
+		HashMap<String, String> serverDetails = new HashMap();
+		HashMap<String, BankServerImpl> serverDirectory = new HashMap();
+		
+		String[] serverList = {"BC", "MB", "NB", "QC"};
+			
 		try
 		{
-			String firstname1 = "Paul";
-			String lastName1 = "Netto";
-			String address1 = "4965, Heritage Drive";
-			String phone1 = "450-123-4567";
-			String customerID1 = "BCCN4567";
-			BranchID branch1 = BranchID.QC;
+			Properties properties = new Properties();
 			
-			EditRecordFields editAddress1 = EditRecordFields.address;
-			String newAddressValue1 = "3921 rue Ellice";
-			EditRecordFields editPhone1 = EditRecordFields.phone;
-			String newPhoneValue1 = "438-123-123"; //Should produce error
+			properties.put("org.omg.CORBA.ORBInitialHost", "127.0.0.1");
+			properties.put("org.omg.CORBA.ORBInitialPort", "1050");
 			
-			//BankServerImpl serverTest1 = new BankServerImpl(branch1.toString(), 1099);
+			//1. Create and initialize ORB
+			ORB orb = ORB.init(args, properties);
 			
-			//serverTest1.createAccount(firstname1, lastName1, address1, phone1, customerID1, branch1);
-			//serverTest1.deposit(customerID1, 1000);
-			//serverTest1.editRecord(customerID1, editAddress1, newAddressValue1);
-			//serverTest1.editRecord(customerID1, editPhone1, newPhoneValue1);			
+			//2. Get reference to RootPOA and activate the POAManager
+			POA rootPOA = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+			rootPOA.the_POAManager().activate();
+			
+			//3.0 Get the root naming context.
+			//3.1 NameService invokes the name service 
+			org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
+			
+			//4.0 Use NamingContextExt which is part of the Interoperable
+			//4.1 Naming Service (INS) specification.
+			NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+			
+			//5. Create servers details
+			serverDetails.put("BC", "localhost:30100");
+			serverDetails.put("MB", "localhost:30200");
+			serverDetails.put("NB", "localhost:30300");
+			serverDetails.put("QC", "localhost:30400");
+			
+			//6.0 Put the servers details in a HashMap
+			//6.1 Create the servers
+			BankServerImpl serverBC = new BankServerImpl(BranchID.BC, orb, "localhost", 30100, serverDetails);
+			BankServerImpl serverMB = new BankServerImpl(BranchID.MB, orb, "localhost", 30200, serverDetails);
+			BankServerImpl serverNB = new BankServerImpl(BranchID.NB, orb, "localhost", 30300, serverDetails);
+			BankServerImpl serverQC = new BankServerImpl(BranchID.QC, orb, "localhost", 30400, serverDetails);
+			
+			serverDirectory.put("BC", serverBC);
+			serverDirectory.put("MB", serverMB);
+			serverDirectory.put("NB", serverNB);
+			serverDirectory.put("QC", serverQC);
+			
+			for(String serverName : serverList)
+			{
+				//7. Get object reference from the servant
+				org.omg.CORBA.Object ref = rootPOA.servant_to_reference(serverDirectory.get(serverName));
+				
+				//8. Cast the reference to a CORBA reference
+				BankServerInterface href = BankServerInterfaceHelper.narrow(ref);
+				
+				//9. Bind the Object Reference in Naming
+				NameComponent path[] = ncRef.to_name(serverName);
+				ncRef.rebind(path, href);
+				
+				System.out.println("ServerDriver Log: | Bank Server: " + serverName + " initialized." );
+			}
+			
+			//10. Wait for invocation from clients
+			orb.run();	
 		}
 		catch (Exception e)
 		{
-			System.err.println("Server Driver Log: Error: Server initialization failure.");
-			System.err.println(e.getMessage());
-			e.printStackTrace();
+			System.err.println("ServerDriver Log: | Error: " + e.getMessage());
+			e.printStackTrace(System.out);
 		}
 	}
 }
